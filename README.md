1cd8e0f81e4c9280ff797560c5369fed024395c5
Kienan Leung <leung.kienan@gmail.com>
2023-03-02 16:20:20 +1300

    changing state with search function

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # MangList
+
 An alternative version of "MyAnimeList" where I interact with the Manga side of the site in a modern format.
 
 # Starting the server
+
 Run the following command to launch the app:
+
 ```
 npm start
 ```
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,73 +1,51 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import { useState, useRef } from 'react'
 import './style.css'
+
+// function useOutside(ref) {
+//   useEffect(() => {
+//     function handleOutsideClick(e) {
+//       if (ref.current && !ref.current.contains(e.target)) {
+//         return (
+//           <div id="search" className="menu-item">
+//             <button id="newSearch" onClick={() => searchWindow(true)}>
+//               Search
+//             </button>
+//           </div>
+//         )
+//       }
+//     }
+//     // Bind the event listener
+//     document.addEventListener('mousedown', handleOutsideClick)
+//     return () => {
+//       // Unbind the event listener on clean up
+//       document.removeEventListener('mousedown', handleOutsideClick)
+//     }
+//   }, [ref])
+// }
 
 export default function Search() {
   const [searched, searchWindow] = useState(false)
 
-  // implementing a off click state change
-
-  //   const handleClickOutside = () => {
-  //     searchWindow(false)
-  //   }
-
-  //   const ref = useOutsideClick(handleClickOutside)
-
-  //   const handleClick = () => {
-  //     searchWindow((state) => {
-  //       if (state) {
-  //         return (
-  //           <div>
-  //             <input id="searchField" placeholder="Enter a manga..."></input>
-  //             <span id="searchButton" className="material-symbols-outlined">
-  //               search
-  //             </span>
-  //           </div>
-  //         )
-  //       }
-  //     })
-  //   }
-
-  //   const handleHeaderClick = (e) => {
-  //     e.stopPropagation()
-  //   }
-
-  //   return (
-  //     <div onClick={handleHeaderClick}>
-  //       <button ref={ref} id="newSearch" onClick={handleClick}>
-  //         Search
-  //       </button>
-  //     </div>
-  //   )
-
-  //   const outsideClick = (callback) => {
-  //     const ref = useRef()
-  //     useEffect(() => {
-  //       const handleClick = (e) => {
-  //         if (ref.current && !ref.current.contains(e.target)) callback()
-  //       }
-  //       document.addEventListener('click', handleClick)
-  //       return () => {
-  //         document.removeEventListener('click', handleClick)
-  //       }
-  //     }, [ref])
-  //     return ref
-  //   }
-
   if (searched) {
-    // send query to backend which taps into jikan api to return results.
     return (
-      <div>
-        <input id="searchField" placeholder="Enter a manga..."></input>
-        <span id="searchButton" className="material-symbols-outlined">
-          search
-        </span>
+      <div id="search" className="menu-item">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            alert('sending')
+          }}
+        >
+          <input id="searchField" placeholder="Enter a manga..."></input>
+        </form>
       </div>
     )
   }
   return (
-    <button id="newSearch" onClick={() => searchWindow(true)}>
-      Search
-    </button>
+    <div id="search" className="menu-item">
+      <button id="newSearch" onClick={() => searchWindow(true)}>
+        Search
+      </button>
+    </div>
   )
 }
